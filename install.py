8b2e3088d01ab27dfdd81ac4fb76b2c8e6458c93
Taff Gao <gaotongfei1995@gmail.com>
2016-10-17 20:14:43 +0800

    update

--- install.py
+++ install.py
@@ -3,5 +3,5 @@ import os
 
 subprocess.check_call(["git", "clone", "https://github.com/VundleVim/Vundle.vim",
     os.path.expanduser("~/.vim/bundle/Vundle.vim")])
-subprocess.check_call(["git", "clone", "https://github.com/gaotongfei/vimrc.git", 
-    os.path.expanduser("~/")])
+
+subprocess.check_call(["git", "clone", "https://github.com/gaotongfei/vimrc.git"])
