7fc030a27b6e1596cde230e3a3b213e4a8c1bc59
Taff Gao <gaotongfei1995@gmail.com>
2016-10-17 12:47:06 +0000

    update

--- install.py
+++ install.py
@@ -1,7 +1,31 @@
-import subprocess
-import os
+import shutil, errno, os, subprocess
 
-subprocess.check_call(["git", "clone", "https://github.com/VundleVim/Vundle.vim",
-    os.path.expanduser("~/.vim/bundle/Vundle.vim")])
+exists = os.path.exists
+expanduser = os.path.expanduser
 
-subprocess.check_call(["git", "clone", "https://github.com/gaotongfei/vimrc.git"])
+def copy(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc:
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        elif exc.errno == 17:
+            pass 
+
+vundle_folder = expanduser("~/.vim/bundle/Vundle.vim")
+if exists(vundle_folder):
+    shutil.rmtree(vundle_folder)
+
+subprocess.check_call(["git", "clone", "https://github.com/VundleVim/Vundle.vim", vundle_folder])
+
+# moving colors and after folder to .vim folder
+copy('colors', expanduser('~/.vim/colors'))
+copy('after', expanduser('~/.vim/after'))
+
+# moving .vimrc home
+copy('.vimrc', expanduser('~/'))
+copy('.gvimrc', expanduser('~/'))
+
+# install plugins
+
+subprocess.check_call(["vim", "+PluginInstall", "+qall"])
